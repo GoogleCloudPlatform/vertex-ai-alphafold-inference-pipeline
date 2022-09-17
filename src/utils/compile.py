bfade0cb03eb3d2c6162bc787b6a861b44401657
jarokaz <jarokaz@no-reply.com>
2022-09-17 19:07:01 +0000

    Added scripts to compile and submit

---
+++ src/utils/compile.py
@@ -0,0 +1,95 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Compiles a KFP v2 pipeline."""
+
+import importlib
+import os
+
+from absl import flags
+from absl import app
+from absl import logging
+from datetime import datetime
+from importlib import import_module
+
+from google.cloud import aiplatform as vertex_ai
+from google.cloud import filestore_v1
+
+from kfp.v2 import compiler
+
+
+flags.DEFINE_string('project', 'jk-mlops-dev', 'GCP Project')
+flags.DEFINE_string('filestore_instance_id', 'jk-af-dev-fs', 'Filestore instance ID')
+flags.DEFINE_string('filestore_instance_location', 'us-central1-a', 'Filestore instance location')
+flags.DEFINE_string('filestore_share', '/datasets', 'Filestore share')
+flags.DEFINE_string('pipeline_package_path', None, 'A path to the output JSON file for the pipeline IR')
+flags.DEFINE_string('pipeline_fun', None, 'Pipeline function name')
+flags.DEFINE_string('alphafold_components_image', 'gcr.io/jk-mlops-dev/alphafold-components', 'AlphaFold components container image')
+flags.DEFINE_string('model_params_path', 'gs://jk-af-dev-bucket', 'A path to AlphaFold params')
+flags.DEFINE_string('predict_gpu', 'nvidia-tesla-t4', 'GPU type for prediction')
+flags.DEFINE_string('relax_gpu', 'nvidia-tesla-t4', 'GPU type for relaxation')
+
+flags.mark_flag_as_required('pipeline_package_path')
+flags.mark_flag_as_required('pipeline_fun')
+
+FLAGS = flags.FLAGS
+
+
+def _get_fun_by_name(fun_string: str):
+    mod_name, fun_name = fun_string.rsplit('.', 1)
+    mod = importlib.import_module(mod_name)
+    func = getattr(mod, fun_name)
+
+    return func, fun_name
+
+def _get_filestore_info(project_id: str, instance_id: str, location: str):
+    client = filestore_v1.CloudFilestoreManagerClient()
+    instance_name = f'projects/{project_id}/locations/{location}/instances/{instance_id}'
+    response = client.get_instance(name=instance_name)
+
+    return response.networks[0].ip_addresses[0], response.networks[0].network
+
+def _main(argv):
+    
+    ip_address, network = _get_filestore_info(FLAGS.project, 
+                                              FLAGS.filestore_instance_id, 
+                                              FLAGS.filestore_instance_location)
+    os.environ['APHAFOLD_COMPONENTS_IMAGE'] = FLAGS.alphafold_components_image
+    os.environ['NFS_SERVER'] = ip_address
+    os.environ['NFS_PATH'] = FLAGS.filestore_share
+    os.environ['NETWORK'] = network 
+    os.environ['MODEL_PARAMS_GCS_LOCATION'] = FLAGS.model_params_path
+
+    if FLAGS.predict_gpu == 'nvidia-tesla-a100':
+        os.environ['MEMORY_LIMIT'] = '85'
+        os.environ['CPU_LIMIT'] = '12'
+        os.environ['GPU_LIMIT'] = '1'
+        os.environ['GPU_TYPE'] = 'nvidia-tesla-a100'
+
+    if FLAGS.relax_gpu == 'nvidia-tesla-a100':
+        os.environ['RELAX_MEMORY_LIMIT'] = '85'
+        os.environ['RELAX_CPU_LIMIT'] = '12'
+        os.environ['RELAX_GPU_LIMIT'] = '1'
+        os.environ['RELAX_GPU_TYPE'] = 'nvidia-tesla-a100'
+
+    pipeline_func, _ = _get_fun_by_name(FLAGS.pipeline_fun) 
+    compiler.Compiler().compile(
+        pipeline_func=pipeline_func,
+        package_path=FLAGS.pipeline_package_path
+    )
+
+
+
+if __name__ == "__main__":
+    app.run(_main)
---
+++ src/utils/run.py
@@ -0,0 +1,115 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Submits a pipeline run."""
+
+import gcsfs
+import fsspec
+import os
+
+from absl import flags
+from absl import app
+from absl import logging
+from datetime import datetime
+from importlib import import_module
+
+from google.cloud import aiplatform as vertex_ai
+
+
+
+flags.DEFINE_string('project', 'jk-mlops-dev', 'GCP Project')
+flags.DEFINE_string('region', 'us-central1', 'Vertex Pipelines region')
+flags.DEFINE_string('staging_bucket', None, 'Staging bucket')
+flags.DEFINE_string('pipelines_sa', 'pipelines-sa@jk-mlops-dev.iam.gserviceaccount.com', 'Pipelines SA')
+flags.DEFINE_string('pipeline_template_path', None, 'A path to the output JSON file for the pipeline IR')
+flags.DEFINE_string('params', None, 'Runtime parameters')
+flags.DEFINE_string('experiment_id', None, 'Experiment ID')
+flags.DEFINE_bool('enable_caching', False, 'Enable pipeline level caching')
+
+flags.mark_flag_as_required('pipeline_template_path')
+flags.mark_flag_as_required('params')
+
+FLAGS = flags.FLAGS
+
+
+def _maybe_bool(value: str):
+    if value == 'True':
+        value = True
+    if value == 'False':
+        value = False
+    return value
+
+
+def _convert_params(param_string: str) -> dict:
+    params = {param.split('=')[0]: _maybe_bool(param.split('=')[1])
+              for param in param_string.split(',')}
+    return params
+
+
+def _copy_sequence(local_path: str, gcs_path: str):
+    local_fs = fsspec.filesystem('file')
+    gcs_fs = gcsfs.GCSFileSystem()
+
+    gcs_fs.put(local_path, gcs_path)
+
+
+
+def _main(argv):
+    params = _convert_params(FLAGS.params)
+
+    if not os.path.exists(params['sequence_path']):
+        raise FileNotFoundError('Invalid sequence path')
+
+    if not os.path.exists(FLAGS.pipeline_template_path):
+        raise FileNotFoundError('Invalid path to pipeline JSON')
+
+    sequence_file_name = os.path.basename(params['sequence_path'])
+    gcs_sequence_path = f'{FLAGS.staging_bucket}/fasta/{sequence_file_name}'
+
+    logging.info(f'Copying {params["sequence_path"]} to {gcs_sequence_path}')
+    _copy_sequence(params['sequence_path'], gcs_sequence_path)
+    params['sequence_path'] = gcs_sequence_path
+
+    vertex_ai.init(
+        project=FLAGS.project,
+        location=FLAGS.region,
+        staging_bucket=FLAGS.staging_bucket,
+    )
+
+    pipeline_name = os.path.basename(FLAGS.pipeline_template_path).split('.')[0].lower()
+    labels = {
+        'experiment_id': FLAGS.experiment_id,
+        'sequence_id': sequence_file_name.split('.')[0].lower()
+    }
+
+    pipeline_job = vertex_ai.PipelineJob(
+        display_name=pipeline_name,
+        template_path=FLAGS.pipeline_template_path,
+        pipeline_root=f'{FLAGS.staging_bucket}/pipeline_runs/{pipeline_name}',
+        parameter_values=params,
+        enable_caching=FLAGS.enable_caching,
+        labels=labels
+    )
+
+    pipeline_job.run(
+        sync=False,
+        service_account=FLAGS.pipelines_sa)
+
+   
+
+
+
+
+if __name__ == "__main__":
+    app.run(_main)
